634a85fae7ff8e5653e0e503965ff04bbd0c6f1b
Nathan Lee <nathan.l.lee123@gmail.com>
2020-08-23 21:34:15 -0700

    Refined editor

--- screens/Editor.js
+++ screens/Editor.js
@@ -7,6 +7,16 @@ import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 import firebase from '../config/Firebase';
 import 'firebase/firestore';
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default class Editor extends React.Component {
     state = {
@@ -54,6 +64,10 @@ export default class Editor extends React.Component {
             Sender: "dOd8rPhPQGPIO9uN2ZvyeLq5uuf1",
         });
         console.log("url: ", url);
+
+        this.setState({image: null});
+        alert("Image submitted")
+
         return url
     }
     /*
